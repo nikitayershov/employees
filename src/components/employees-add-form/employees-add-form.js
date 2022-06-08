20ff4a1c4b9811e1af16e93a0cf37f2aac660429
Nikita Yershov <nikita.yershov@gmail.com>
2022-06-08 14:07:41 +0300

    134. Работа с формами, управляемые и неуправляемые компоненты

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,23 +1,52 @@
+import {Component} from "react";
+
 import './employees-add-form.css'
 
-const EmployeesAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Добавьте нового сотрудника</h3>
-            <form
-                className="add-form d-flex">
-                <input type="text"
-                       className="form-control new-post-label"
-                       placeholder="Как его зовут?" />
-                <input type="number"
-                       className="form-control new-post-label"
-                       placeholder="З/П в $?" />
+class EmployeesAddForm extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            salary: ''
+        }
+    }
 
-                <button type="submit"
-                        className="btn btn-outline-light">Добавить</button>
-            </form>
-        </div>
-    )
+    onValueChange = (e) => {
+       this.setState({
+           [e.target.name]: e.target.value // позволяет динамически изменять поля для state (key нужно записывать в [])
+       })
+    }
+
+    render() {
+        const {name, salary} = this.state
+        return (
+            <div className="app-add-form">
+                <h3>Добавьте нового сотрудника</h3>
+                <form
+                    className="add-form d-flex">
+                    <input type="text"
+                           className="form-control new-post-label"
+                           placeholder="Как его зовут?"
+                           name="name" // должно соответствовать ключам state. И
+                           value={name} // если value стоит в том ключе что мы здесь используем state, то в value з
+                            // аписывается актуальное значение компонента, то есть значение value формы инпута будет
+                            // контроироваться React-ом и элемент будет называться управляемым компонентом ( в данном
+                            // случае управяемый элемент
+                            // получается двойное связывание: то что иы вводим в input контролируется при помощи state
+                            // который записывается в компонент
+                           onChange={this.onValueChange}/>
+                    <input type="number"
+                           className="form-control new-post-label"
+                           placeholder="З/П в $?"
+                           name="salary"
+                           value={salary}
+                           onChange={this.onValueChange}/>
+                    <button type="submit"
+                            className="btn btn-outline-light">Добавить</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default EmployeesAddForm;
