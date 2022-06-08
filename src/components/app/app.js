a214cbdb90de89e2a222d0ed3779d84b11437ee3
Nikita Yershov <nikita.yershov@gmail.com>
2022-06-08 21:28:41 +0300

    134. Работа с формами, управляемые и неуправляемые компоненты

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import {Component} from "react";
+
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
@@ -6,28 +8,46 @@ import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
 import './app.css';
 
-function App() {
+class App extends Component {
 
-    const data = [
-        {name: "John C.", salary: 800, increase: true, id: 1},
-        {name: "Alex M.", salary: 3000, increase: false, id: 2},
-        {name: "Carl W.", salary: 15000, increase: true, id: 3}
-    ]
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: "John C.", salary: 800, increase: true, id: 1},
+                {name: "Alex M.", salary: 3000, increase: false, id: 2},
+                {name: "Carl W.", salary: 15000, increase: true, id: 3}
+            ]
 
-    return (
-        <div className='app'>
-            <AppInfo/>
+        }
+    }
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id) // возвращает новый массив без элемента с указанным id
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className='app'>
+                <AppInfo/>
+
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+
+                <EmployeesList
+                    data={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <EmployeesAddForm/>
             </div>
-
-
-            <EmployeesList data={data}/>
-            <EmployeesAddForm/>
-        </div>
-    )
+        )
+    }
 }
+
 
 export default  App;
